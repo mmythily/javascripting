a912f9d313b8a83564560c3f48bda1f0235ea8d4
Mythily <mythily.mudunuru@gmail.com>
2019-04-04 05:23:51 +0000

    All the solutions to my javascripting workshop

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+var fruit;
+//Make the fruit variable reference the value orange with the type of String
+fruit = 'orange';
+if (fruit.length > 5){
+  console.log("The fruit name has more than five characters.");
+}
+else{
+  console.log("The fruit name has less than five characters.");
+}
---
+++ rounding-numbers.js
@@ -0,0 +1,4 @@
+var roundUp = 1.5;
+//Math.round() - round the number up
+var rounded = Math.round(roundUp);
+console.log(rounded);
